51c11b98cc6080f01dddccda8b3489b5049154ca
mediocha <dr.ant.madson@gmail.com>
2023-09-03 12:43:43 -0300

    finalizacao.

--- public/js/index.js
+++ public/js/index.js
@@ -1,88 +1,61 @@
-document.getElementById("bar-menu").innerHTML = "Hello JavaScript!";
+/**
+ * 
+ * Numero de Variaveis: input-var
+ * Função-Objetivo: 
+ * 
+ * Quantidade de Restrições: input-restr
+ * 
+ * Botão Gerar: gerar-pll
+ */
+var inputs = $('input').on('keyup', verificarInputs);
 
-
-function gerarPLL(msg){
-
-    let num_var = document.getElementById("var").value;
-    let num_restr = document.getElementById("restr").value;
-    let value_checked = document.querySelector('input[type=radio]:checked').value;
-
-    document.querySelector('input[type=radio]:checked').value
-    
-    let msga = (num_var + ' ' + ' ' + num_restr + ' ' + value_checked);
-
-    document.getElementById("info-right").innerHTML = msga;
+function verificarInputs() {
+    const preenchidos = inputs.get().every(({value}) => value)
+    $('button').prop('disabled', !preenchidos);
 }
 
-$("#rowAdder").click(function () {
-    newRowAdd =
-    '<div class="row"><div class="col"> <div class="input-group">' +
-    '<input type="text" class="form-control m-input"> </div> </div>' +
-    '<div class="col"><button class="btn btn-danger" id="DeleteRow" type="button">' +
-    '<i class="bi bi-trash"></i> Delete</button></div></div>';
+$("#gerar-pll").click(function () {
 
-    $('#newinput').append(newRowAdd);
-});
-$("body").on("click", "#DeleteRow", function () {
-    $(this).parents(".row").remove();
-})
+    let num_var = document.getElementById("input-var").value;
+    let restr_var = document.getElementById("input-restr").value;
+    var ch = $("#radio-form input[type='radio']:checked").val();
 
-$("#rowAdderFo").click(function () {
-    newRowAddFo =
-    '<div class="col col-md-2"> <div class="input-group">' +
-    '<input type="text" class="form-control m-input"> </div> </div>';
-
-    $('#input-fo').append(newRowAddFo);
-});
-
-$("#fo-btn").click(function() {
-    let num_var = document.getElementById("fo").value;
-    
-    
-    // colunas antes do igual
+    var i = 0, j = 0;
     coluna = "";
-    cont = parseInt(num_var);
-    var i=0;
-
-    // numero de variaveis
-    /*for(i = 0; i < 3; i++)
-    {
-        coluna += '<div class="col"> <div class="input-group">' +
-        '<input type="text" id="col-fo-0' + i + '" class="form-control m-input"> </div> </div>';
-    
-    }
-    // pensar num jeito dos ids não ficarem iguais
-    // select
-    coluna += '<div class="col"> <select class="form-control" id="select-fo">' +
-              '<option><=</option><option>>=</option>' +
-              '</select></div>'+
-              '<div class="col"> <div class="input-group">' +
-              '<input type="text" id="col-fo-0' + i + '" class="form-control m-input"> </div> </div>';*/
-    //numero de variaveis
-    
     newTeste = "";
-    var j = 0;
 
-    // restrições
-    for(i = 0; i < parseInt(num_var); i++){
+    newRowAdd = '<br><label for="var-decisao">Variavéis de Decisão</label><br />';
+    newRowAddFo = '<br><label for="func-obj">Função-objetivo</label><br />' + 
+    '<div class="row"><div class="col col-md-2"><div style="font-weight:bold; text-align: center; align-items: center;">' + ch.toUpperCase() + ' Z' + '</div></div>';
+    newTeste = '<br><label for="restricao">Restrições</label><br />';
 
+    for(i = 0; i < parseInt(num_var); i++)
+    {
+        newRowAdd +=
+        '<div class="row"><div class="col col-md-8"> <div class="input-group">' +
+        '<input type="text" class="form-control m-input"> </div> </div></div>';
+
+        newRowAddFo +=
+        '<div class="col col-md-2"> <div class="input-group">' +
+        '<input type="text" id="fo-' + i +'" class="form-control m-input"> </div> </div>';
+    
         newTeste += '<div class="row" id="row-0' + i +'">';// + coluna+ '</div>';
 
-        for(j = 0; j <= 3; j++){
+        for(j = 0; j <= parseInt(restr_var); j++){
 
             //if(cont < 3)
             //variaveis
-            if(j == 3){
+            if(j == parseInt(restr_var)){
 
-                coluna += '<div class="col"> <select class="form-control" id="select-fo">' +
-              '<option><=</option><option>>=</option>' +
-              '</select></div>'+
-              '<div class="col"> <div class="input-group">' +
-              '<input type="text" id="col-fo-0' + i + j+ '" class="form-control m-input"> </div> </div>';
+                coluna += '<div class="col"> <select class="form-control" id="select-fo-0'+ i + '">' +
+                '<option><=</option><option>>=</option><option>=</option>' +
+                '</select></div>'+
+                '<div class="col"> <div class="input-group">' +
+                '<input type="text" id="col-fo-0' + i + j+ '" class="form-control m-input"> </div> </div>';
 
             }else{
                 coluna += '<div class="col"> <div class="input-group">' +
-            '<input type="text" id="col-fo-0' + i + j + '" class="form-control m-input"> </div> </div>';
+                '<input type="text" id="col-fo-0' + i + j + '" class="form-control m-input"> </div> </div>';
             }
             
         }
@@ -91,43 +64,59 @@ $("#fo-btn").click(function() {
         $('#lista-restricoes').append(newTeste);
         coluna="";
         newTeste="";
+        
     }
-
-    // newTeste = "";
-    // for(i = 0; i < parseInt(num_var); i++)
-    // {
-    //     newTeste += '<div class="row" id="row-0' + i +'">' + coluna+ '</div>';
-    // }
-
     
-    
+    $('#newinput').append(newRowAdd);
+
+    newRowAddFo+='</div>';
+
+    $('#input-fo').append(newRowAddFo);
+
+    buttonDualGerador = '<div class="row"><div class="col">' +
+    '<button id="gerar-dual" type="button" class="btn btn-red">' +
+    'Dual</button></div> </div>';
+
+    $('#dual-btn').append(buttonDualGerador);
 
 });
+// $("body").on("click", "#DeleteRow", function () {
+//     $(this).parents(".row").remove();
+// });
 
-$("#dual-btn").click(function () {
-    let num_var = document.getElementById("fo").value;
-    var value_checked = $("input:radio:checked").text();
+// <div class="col"><button class="btn btn-danger" id="DeleteRow" type="button">' +
+//         '<i class="bi bi-trash"></i> Delete</button></div></div>'
+
+$("#dual-btn").on("click","#gerar-dual" ,function () {
+
+    // let num_var = document.getElementById("fo").value;
+    // var value_checked = $("input:radio:checked").text();
+    // var ch = $("#radio-form input[type='radio']:checked").val();
+    let num_var = document.getElementById("input-var").value;
+    let restr_var = document.getElementById("input-restr").value;
     var ch = $("#radio-form input[type='radio']:checked").val();
-    
+    var selectDesig = "";
+
     if(ch === "max")
     {
         ch= "MIN";
-
-    }
-    else
+    }else
     {
         ch = 'MAX';
     }
 
     // ultima coluna de restrições em todas as linhas
     var preenchT = "";
+    preenchRest = "";
+    preenchRestFinal = "";
+    gerarDualBTN = "";
 
     // IMPORTANTE: substituir o numero 3, pois se trata do numero de variaveis
     for(i = 0; i < parseInt(num_var); i++){
 
         //let alok = document.getElementById("#col-fo-003").value;
-        var alok = $('#col-fo-0'+ i + '3').val();
-        alert(alok);
+        var alok = $('#col-fo-0'+ i + parseInt(restr_var)).val();
+        
         preenchT += alok + 'x' + (i+1);
 
         if( i < parseInt(num_var)-1){
@@ -136,19 +125,108 @@ $("#dual-btn").click(function () {
 
     }
 
-
-    newRowAddFo =
+    gerarDualBTN +=
     '<div class="row"> <div class="col">' +
-    '<p> Função-Objetivo: ' + ch + ' C = ' + preenchT + '</p></div>' +
-    '<p> Restrições: <br> ' +
-    '</div>';
+    '<p> Função-Objetivo: ' + ch + ' W = ' + preenchT + '</p></div></div>';
+    
+    gerarDualBTN+= 
+    '<div class="row"> <div class="col">' +
+    '<p> Restrições </p></div></div>';
+
+    selectFinal = [];
+    var cert = 0;
+    var ind = [];
+    var cod;
+    for(i = 0; i < parseInt(num_var); i++){
+
+        //<= MAX
+        //>= MIN
+        // = depende
+
+        selectDesig = $('#select-fo-0'+i).find(":selected").text();
+
+        if(ch === "MAX" && selectDesig === "<=")
+        {
+            cert = 1;
+            ind.push(i);
+            cod = ' >= ';
+
+        }else if(ch === "MIN" && selectDesig === ">="){
+        
+            cert = 1;
+            ind.push(i);
+            cod = ' <= ';
+        }
+    }
+     //alert(selectFinal);
+    
+     // adição da desigualdade e reforço
+   // cont = parseInt(num_var);
+    for(i = 0; i < parseInt(restr_var); i++){
+
+        preenchRest = '<div class="row"> <div class="col">';
+        
+        
+        for(j = 0; j < parseInt(num_var); j++){
+            // 4 5 6 >= 5
+            // 7 8 9 >= 2
+            selectDesig = $('#select-fo-0'+j).find(":selected").text();
+
+             
+            if(ch === "MAX" && selectDesig === ">="){
+                cod = ' <= ';
+            }else if(ch === "MIN" && selectDesig === "<="){
+                cod = ' >= ';
+            }else if(selectDesig === "="){
+                if(ch === "MAX")
+                    cod = ' <= ';
+                else
+                    cod = ' >= ';
+            }
+        
+
+            // se tiver alguma variavel que precisa ser negativa
+            for(k = 0; k < ind.length; k++){
+                if(cert === 1 && j === ind[k]){
+                    preenchRest += ($('#col-fo-0'+ j + i).val()*-1) + 'y' + (j+1) + ' ';
+                    j++;
+                }
+            }
+
+            // iteração de j - passa para proxima linha
+            if(j !== parseInt(num_var)){
+                preenchRest += $('#col-fo-0'+ j + i).val() + 'y' + (j+1);
+            }
+            
+           
+            if( j < parseInt(num_var) - 1){
+                preenchRest+=  ' + ';
+            }
+
+        }
 
 
-    // newRowAddFo =
-    // '<div class="row"> <div class="col col-md-2"> <div class="input-group">' +
-    // '<input type="text" class="form-control m-input"> </div> </div> </div>';
+        preenchRest += cod + $('#fo-' + i).val() +'</div></div>';
 
-    $('#dual-saida').append(newRowAddFo);
+
+        preenchRestFinal +=preenchRest ;
+
+    }
+
+    newRowF = '<div class="row"><div class="col">';
+    for(i = 0; i < parseInt(num_var); i++){
+        newRowF += 'y' + (i+1);
+
+        if(i < parseInt(num_var) - 1){
+            newRowF += ', ';
+        }  
+                
+    }
+    newRowF+= cod + ' 0</div></div>';
+
+    gerarDualBTN+=preenchRestFinal+newRowF;
+
+    $('#dual-saida').append(gerarDualBTN);
 });
 
 // numero de variaveis
